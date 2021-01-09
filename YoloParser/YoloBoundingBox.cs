36cb849b31acf35c31455ecba01bcd2e7bce77a0
Tech AJ <32822650+halanch599@users.noreply.github.com>
2021-01-09 14:37:43 +0300

    Add files via upload

---
+++ YoloParser/YoloBoundingBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SemanticSegmentationNET.YoloParser
+{
+    public class BoundingBoxDimensions : DimensionsBase { }
+    public class YoloBoundingBox
+    {
+        public BoundingBoxDimensions Dimensions { get; set; }
+
+        public string Label { get; set; }
+
+        public float Confidence { get; set; }
+
+        public RectangleF Rect
+        {
+            get { return new RectangleF(Dimensions.X, Dimensions.Y, Dimensions.Width, Dimensions.Height); }
+        }
+
+        public Color BoxColor { get; set; }
+    }
+}
